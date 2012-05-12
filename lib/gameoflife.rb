5aeec1a3d2239e03ce167c4ccde372f73ed5ce0a
rockyj <rockyj@rocky-vaio-linux.(none)>
2012-05-12 22:56:17 +0530

    More cleanup!

--- lib/gameoflife.rb
+++ lib/gameoflife.rb
@@ -7,16 +7,11 @@ require_relative 'gameoflife/matrix'
 module Gameoflife
 
   private
-  def self.run_the_game_infinitely(matrix)
-    game_engine = GameEngine.new()
-    matrix_printer = MatrixPrinter.new()
-    
+  def self.run_the_game_infinitely(matrix, game_engine, matrix_printer)
     while(true) do
       puts ""
-      
       matrix_printer.print(matrix)
       matrix = game_engine.get_next_generation(matrix)
-      
       sleep 3
     end
   end
@@ -24,11 +19,13 @@ module Gameoflife
   puts "Welcome to the Game of Life!! Press Ctrl+C to exit anytime.."
   
   game_input = GameInput.new
+  game_engine = GameEngine.new
+  matrix_printer = MatrixPrinter.new
   
   height, width = game_input.initialize_matrix_height_and_width
   seeds = game_input.initialize_seeds
   matrix = game_input.setup_matrix(height, width, seeds)
   
-  run_the_game_infinitely(matrix)
+  run_the_game_infinitely(matrix, game_engine, matrix_printer)
   
 end

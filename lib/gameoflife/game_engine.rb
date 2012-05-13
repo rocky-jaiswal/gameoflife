154f1f4bc81991cc5216387204ead574ceb95167
rockyj <rockyj@rocky-vaio-linux.(none)>
2012-05-13 13:00:07 +0530

    cleanup!

--- lib/gameoflife/game_engine.rb
+++ lib/gameoflife/game_engine.rb
@@ -14,21 +14,26 @@ module Gameoflife
     end
     
     def get_next_generation(matrix)
+      next_gen_alive_cells = get_next_generation_alive_cells(matrix)
+      next_gen_alive_cells.map{|cell| cell.alive = true}
+      @game_input.cells = next_gen_alive_cells
+      matrix = @game_input.setup_matrix
+      return matrix
+    end
+    
+    private
+
+    def get_next_generation_alive_cells(matrix)
       next_gen_alive_cells = []
       
       matrix.cells.each do |cell|
         alive_neighbour_count = @neighbourhood.get_alive_neighbour_count(matrix, cell)
         next_gen_alive_cells << cell if should_live?(cell, alive_neighbour_count)
       end
-      
-      next_gen_alive_cells.map{|cell| cell.alive = true}
-      @game_input.cells = next_gen_alive_cells
-      matrix = @game_input.setup_matrix
-      
-      return matrix
+
+      return next_gen_alive_cells
     end
-    
-    private
+
     def should_live?(cell, alive_neighbour_count)
       live = false
       @rules.each{|rule| live = live || rule.send(:execute, cell, alive_neighbour_count)}
